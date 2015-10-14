e02286cfe08394bcda0a24dd4bb96c4c7ece9dfa
Joa <rocketyjoa@hotmail.com>
2015-10-14 00:09:29 -0300

    Update TutorialWorldGenerator.java

--- com/joa/techmod/world/TutorialWorldGenerator.java
+++ com/joa/techmod/world/TutorialWorldGenerator.java
@@ -24,7 +24,20 @@ public class TutorialWorldGenerator implements IWorldGenerator {
 		this.gen_cyanite_ore = new WorldGenMinable(ModBlocks.cyanite_ore.getDefaultState(), 9);
 		this.gen_zestoren_ore = new WorldGenMinable(ModBlocks.zestoren_ore.getDefaultState(), 5, BlockHelper.forBlock(Blocks.netherrack));
 	}
+	
+	private void runGenerator(WorldGenerator generator, World world, Random rand, int chunk_X, int chunk_Z, int chancesToSpawn, int minHeight, int maxHeight) {
+		if (minHeight < 0 || maxHeight > 256 || minHeight > maxHeight)
+			throw new IllegalArgumentException("Illegal Height Arguments for WorldGenerator");
 
+		int heightDiff = maxHeight - minHeight + 1;
+		for (int i = 0; i < chancesToSpawn; i++) {
+			int x = chunk_X * 16 + rand.nextInt(16);
+			int y = minHeight + rand.nextInt(heightDiff);
+			int z = chunk_Z * 16 + rand.nextInt(16);
+			generator.generate(world, rand, new BlockPos(x, y, z));
+		}
+	}
+	
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) {
 		switch (world.provider.getDimensionId()) {
@@ -39,18 +52,4 @@ public class TutorialWorldGenerator implements IWorldGenerator {
 			break;
 		}
 	}
-
-	private void runGenerator(WorldGenerator generator, World world, Random rand, int chunk_X, int chunk_Z, int chancesToSpawn, int minHeight, int maxHeight) {
-		if (minHeight < 0 || maxHeight > 256 || minHeight > maxHeight)
-			throw new IllegalArgumentException("Illegal Height Arguments for WorldGenerator");
-
-		int heightDiff = maxHeight - minHeight + 1;
-		for (int i = 0; i < chancesToSpawn; i++) {
-			int x = chunk_X * 16 + rand.nextInt(16);
-			int y = minHeight + rand.nextInt(heightDiff);
-			int z = chunk_Z * 16 + rand.nextInt(16);
-			generator.generate(world, rand, new BlockPos(x, y, z));
-		}
-	}
-
 }
